33b25a4eadbbaeba7f52649c1d49be7ad1bce148
Donghee Han <hdh7485@naver.com>
2018-02-23 17:47:29 +0900

    Modify state_machine_simple.launch

--- scripts/state_machine_simple.py
+++ scripts/state_machine_simple.py
@@ -8,6 +8,7 @@ from keyboard.msg import Key
 
 mission_code = '\0'
 
+"""
 class KeyListener():
     def __init__(self):
         self.key_sub = rospy.Subscriber('keyboard/keydown', Key, self.keyboard_cb)
@@ -16,18 +17,27 @@ class KeyListener():
         mission_code = char(data.code)
         print(mission_code)
         return mission_code
+"""
 
 #define state Foo
 class Foo(smach.State):
     def __init__(self):
         smach.State.__init__(self, outcomes=['outcome1', 'outcome2'])
-        self.counter = 0
+
+    def keyboard_cb(self, data):
+        self.mission_code = char(data.code)
+        print(mission_code)
+        return mission_code
 
     def execute(self, userdata):
         rospy.loginfo('Executing state FOO')
-        rospy.sleep(3)
+
+        self.key_sub = rospy.Subscriber('keyboard/keydown', Key, self.keyboard_cb)
+        self.key_value = self.keyboard_cb() 
+        print("key value=")
+        print(key_value)
+
         if self.counter < 3:
-            self.counter += 1
             return 'outcome1'
         else:
             return 'outcome2'
@@ -39,24 +49,23 @@ class Bar(smach.State):
 
     def execute(self, userdata):
         rospy.loginfo('Executing state BAR')
-        rospy.sleep(3)
+        rospy.sleep(2)
         return 'outcome2'
 
 #main
 def main():
     rospy.init_node('smach_example_state_machine')
 
-    myKey = KeyListener()
     #Create a SMACH state machine
     sm = smach.StateMachine(outcomes=['outcome4', 'outcome5'])
 
     #Open the container
     with sm:
-        smach.StateMachine.add('BAR', smach_ros.MonitorState("/keyboard/keydown", Key, keyboard_cb), transitions={'2':'FOO'})
-        smach.StateMachine.add('FOO', smach_ros.MonitorState("/keyboard/keydown", Key, keyboard_cb), transitions={'1':'BAR'})
+        #smach.StateMachine.add('FOO', smach_ros.MonitorState("/keyboard/keydown", Key, keyboard_cb), transitions={'1':'BAR'})
+        #smach.StateMachine.add('BAR', smach_ros.MonitorState("/keyboard/keydown", Key, keyboard_cb), transitions={'2':'FOO'})
         #Add states to the container
-        #smach.StateMachine.add('FOO', Foo(), transitions={'outcome1':'BAR', 'outcome2':'outcome4'})
-        #smach.StateMachine.add('BAR', Bar(), transitions={'outcome2':'FOO'})
+        smach.StateMachine.add('FOO', Foo(), transitions={'outcome1':'BAR', 'outcome2':'outcome4'})
+        smach.StateMachine.add('BAR', Bar(), transitions={'outcome2':'FOO'})
 
     #Execute SMACH plan
     outcome = sm.execute()
